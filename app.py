6b33c2e499e81dcc87ea2c0ea55f73d8d9d98efc
Ramith.s <86591506+rohit7388@users.noreply.github.com>
2025-07-25 21:28:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -39,4 +40,5 @@ def index():
     return render_template("index.html", prediction=prediction, probability=probability)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
